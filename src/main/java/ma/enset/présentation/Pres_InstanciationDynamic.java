531e13e11b150e9973fea2e40c49bc62d977e85e
DalalYouness <younessdalal1@gmail.com>
2025-03-16 11:37:08 +0000

    Instanciation_Dynamic

---
+++ src/main/java/ma/enset/présentation/Pres_InstanciationDynamic.java
@@ -0,0 +1,41 @@
+package ma.enset.présentation;
+
+import ma.enset.dao.DaoImpl;
+import ma.enset.dao.IDao;
+import ma.enset.metier.Imetier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Pres_InstanciationDynamic {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Scanner read = new Scanner(new File("Config.txt"));
+
+        // on va lire le Nom du la class
+        String daoClassName = read.nextLine();
+
+        // on va charger la class dans la memoire
+        Class daoClass = Class.forName(daoClassName);
+
+        /* on va instancier la classe par l'utilisation de la methode
+        pour ne pas modifier dans le code source apres c'est pour ca on va mettre la reference d'objet
+        dans un objet IDao(On connait l'interface et n'ont pas la classe) , on a faire un cast
+        car la methode newInstance Return un object de type Object
+        */
+        IDao dao =  (IDao) daoClass.newInstance();
+
+
+        String metierClassName = read.nextLine();
+        Class cmetier = Class.forName(metierClassName);
+
+        /* on va instancier par le constructeur avec parametre pour faire
+           l'injection de dépendance
+        */
+        Imetier metier = (Imetier) cmetier.getConstructor(IDao.class).newInstance(dao);
+        System.out.println(metier.Calcul());
+
+    }
+}
