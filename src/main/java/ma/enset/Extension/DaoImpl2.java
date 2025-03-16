d10f4d8ba5382f9ad0240a06812a8b3b8a60d359
DalalYouness <younessdalal1@gmail.com>
2025-03-16 11:16:18 +0000

    Add_Extension

---
+++ src/main/java/ma/enset/Extension/DaoImpl2.java
@@ -0,0 +1,12 @@
+package ma.enset.Extension;
+
+import ma.enset.dao.IDao;
+
+public class DaoImpl2 implements IDao {
+    @Override
+    public double getData() {
+        System.out.println("Web Service Version");
+        double Temp = Math.random() * 10;
+        return Temp;
+    }
+}
--- src/main/java/ma/enset/présentation/Pres_InstanciationStatique.java
+++ src/main/java/ma/enset/présentation/Pres_InstanciationStatique.java
@@ -1,13 +1,24 @@
 package ma.enset.présentation;
 
+import ma.enset.Extension.DaoImpl2;
 import ma.enset.dao.DaoImpl;
 import ma.enset.metier.MetierImpl;
 
 public class Pres_InstanciationStatique {
     public static void main(String[] args) {
+        // version database using dependency injection with constructor
+
         DaoImpl dao = new DaoImpl();
         MetierImpl metier = new MetierImpl(dao);
         System.out.println("Result : " + metier.Calcul());
+
+        // version WebService we will test the injection dependency using the setter
+        System.out.println("=====================================");
+
+        DaoImpl2 dao2 = new DaoImpl2();
+        MetierImpl metier2 = new MetierImpl(dao2);
+        System.out.println("Result : " + metier2.Calcul());
+
 
     }
 }
